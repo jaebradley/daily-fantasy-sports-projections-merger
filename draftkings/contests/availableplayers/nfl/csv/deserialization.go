75c3d2a32e897df6196e221c8a4a312cfcfda0f9
Jae Bradley <jae.b.bradley@gmail.com>
2021-10-11 01:53:11 -0400

    Add more deserialization tests

--- draftkings/contests/availableplayers/nfl/csv/deserialization.go
+++ draftkings/contests/availableplayers/nfl/csv/deserialization.go
@@ -11,11 +11,27 @@ type TeamAbbreviationDeserializer struct {
 	teamsByAbbreviation map[string]models.Team
 }
 
-func (d *TeamAbbreviationDeserializer) deserialize(abbreviation string) (*models.Team, error) {
+// Deserialize team abbreviations
+func (d *TeamAbbreviationDeserializer) Deserialize(abbreviation string) (*models.Team, error) {
 	team, exist := d.teamsByAbbreviation[abbreviation]
 	if exist {
 		return &team, nil
 	}
 
 	return nil, fmt.Errorf("No team identified for abbreviation %s", abbreviation)
+}
+
+// ContestPositionDeserializer deserializes abbreviations to contest position
+type ContestPositionDeserializer struct {
+	positionsByAbbreviation map[string]models.ContestPosition
+}
+
+// Deserialize contest position abbreviations
+func (d *ContestPositionDeserializer) Deserialize(abbreviation string) (*models.ContestPosition, error) {
+	position, exist := d.positionsByAbbreviation[abbreviation]
+	if exist {
+		return &position, nil
+	}
+
+	return nil, fmt.Errorf("No position identified for value: %s", abbreviation)
 }
--- draftkings/contests/availableplayers/nfl/csv/deserialization_test.go
+++ draftkings/contests/availableplayers/nfl/csv/deserialization_test.go
@@ -10,8 +10,32 @@ func TestDeserializeUnknownTeamAbbreviation(t *testing.T) {
 	deserializer := TeamAbbreviationDeserializer{
 		make(map[string]models.Team),
 	}
-	team, err := deserializer.deserialize("foo")
+	team, err := deserializer.Deserialize("foo")
 	if nil != team || err == nil {
 		t.Errorf("expected an error")
+	}
+}
+
+func TestDeserializeKnownTeamAbbreviation(t *testing.T) {
+	deserializer := TeamAbbreviationDeserializer{
+		map[string]models.Team{
+			"foo": models.NEWENGLANDPATRIOTS,
+		},
+	}
+
+	team, err := deserializer.Deserialize("foo")
+	if *team != models.NEWENGLANDPATRIOTS || err != nil {
+		t.Errorf("unexpected team or error")
+	}
+}
+
+func TestDeserializeKnownContestPosition(t *testing.T) {
+	deserializer := ContestPositionDeserializer{
+		make(map[string]models.ContestPosition),
+	}
+
+	position, err := deserializer.Deserialize("foo")
+	if nil != position || nil == err {
+		t.Errorf("expected position to be nil and error to exist")
 	}
 }
