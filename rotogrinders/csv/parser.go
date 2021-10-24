c4f36013c8019069285e4dc51a58b14cabb6ce12
Jae Bradley <jae.b.bradley@gmail.com>
2021-10-24 15:34:26 -0400

    Parse rotogrinders projections (#3)

---
+++ rotogrinders/csv/parser.go
@@ -0,0 +1,60 @@
+package csv
+
+import (
+	"bytes"
+	"encoding/csv"
+	"errors"
+	"io"
+
+	coreSerialization "github.com/jaebradley/daily-fantasy-sports-projections-merger/core/serialization"
+	"github.com/jaebradley/daily-fantasy-sports-projections-merger/rotogrinders/models"
+	rotogrindersSerialization "github.com/jaebradley/daily-fantasy-sports-projections-merger/rotogrinders/serialization"
+)
+
+// Deserializer deserializes a File into a mapping of player to the player's contest details
+type Deserializer interface {
+	Deserialize(bytes *bytes.Buffer) (map[models.Player]float64, error)
+}
+
+type Parser struct {
+	PlayerDeserializer     rotogrindersSerialization.PlayerDeserializer
+	ProjectionDeserializer coreSerialization.ProjectionDeserializer
+}
+
+func (p *Parser) Deserialize(bytes *bytes.Buffer) (map[models.Player]float64, error) {
+	projectionsByPlayer := make(map[models.Player]float64)
+	reader := csv.NewReader(bytes)
+
+	_, err := reader.Read()
+
+	if err == io.EOF {
+		return projectionsByPlayer, nil
+	}
+
+	if err != nil {
+		return nil, err
+	}
+
+	for {
+		record, err := reader.Read()
+
+		if err == io.EOF {
+			return projectionsByPlayer, nil
+		}
+
+		if err != nil {
+			return nil, err
+		}
+		player, err := p.PlayerDeserializer.Deserialize(record[0], record[4])
+		_, exist := projectionsByPlayer[*player]
+		if exist {
+			return nil, errors.New("duplicate player found")
+		}
+		projection, err := p.ProjectionDeserializer.Deserialize(record[5])
+		if nil != err {
+			return nil, err
+		}
+		projectionsByPlayer[*player] = projection
+	}
+	return projectionsByPlayer, nil
+}
---
+++ rotogrinders/serialization/deserializers.go
@@ -0,0 +1,32 @@
+package serialization
+
+import (
+	coreSerialization "github.com/jaebradley/daily-fantasy-sports-projections-merger/core/serialization"
+	"github.com/jaebradley/daily-fantasy-sports-projections-merger/rotogrinders/models"
+)
+
+type PlayerDeserializer interface {
+	Deserialize(id string, name string) (*models.Player, error)
+}
+
+type DefaultPlayerDeserializer struct {
+	IDDeserializer   coreSerialization.PlayerIDDeserializer
+	NameDeserializer coreSerialization.PlayerNameDeserializer
+}
+
+func (d *DefaultPlayerDeserializer) Deserialize(id string, name string) (*models.Player, error) {
+	playerID, err := d.IDDeserializer.Deserialize(id)
+	if nil != err {
+		return nil, err
+	}
+
+	playerName, err := d.NameDeserializer.Deserialize(name)
+	if nil != err {
+		return nil, err
+	}
+
+	return &models.Player{
+		ID:   playerID,
+		Name: playerName,
+	}, nil
+}
